bed55fc6e68f4302294cd2f3ceda5048da86f38c
Howard King <rjxx007@sina.com>
2014-05-14 08:27:25 +0800

    增加了在shell中使用调试命令查看程序执行过程

--- printf.sh
+++ printf.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 #FileName: printf.sh
 
+set -x
 printf "%-5s %-10s %-4s\n" No Name Mark
+set +x
 printf "%-5s %-10s %-4.2f\n" 1 Sarath 50.2345
+set -v
 printf "%-5s %-10s %-4.2f\n" 2 James 90.9163
-
+set +v
+printf "%-5s %-10s %-4.2f\n" 3 Howard 100.001
