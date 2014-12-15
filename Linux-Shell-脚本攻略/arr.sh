18a3712ee3344183ae4ddafc88073a71d0e44f34
Howard King <tianlangroot@gmail.com>
2014-12-15 15:46:08 +0800

    correct one syntax error

--- Linux-Shell-脚本攻略/arr.sh
+++ Linux-Shell-脚本攻略/arr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-array_var={1 2 3 4 5 6}
+array_var=(1 2 3 4 5 6)
 array_var[0]="test1"
 array_var[2]="test2"
 array_var[3]="test3"
@@ -13,3 +13,4 @@ echo ${array_var[0]}
 echo ${array_var[*]}
 echo ${array_var[@]}
 echo ${#array_var[*]}
+echo ${#array_var}
